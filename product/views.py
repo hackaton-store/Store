ff15dcd8ec95ab2055ddcdf1cd52c9fe6d5e7d26
xDD <baisal2407@gmail.com>
2023-06-07 13:13:52 +0600

    upgraded products; used permissions

--- product/views.py
+++ product/views.py
@@ -1,27 +1,49 @@
 from rest_framework.viewsets import ModelViewSet
-from django_filters import rest_framework as filters
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.request import Request
 
+from django_filters import rest_framework as filters
+
+from permissions.permissions import IsOwner, IsModerator
 from product.models import Car
-from product.serializers import CarSerializer, CarFilter
+from product.serializers import CarSerializer
 
+
+
+def check_user(request: Request):
+        if not request.user.is_anonymous:
+            if (request.user.is_staff or request.user.is_moderator):
+                return True
 
 
 class CarViewSet(ModelViewSet):
-    queryset = Car.objects.filter(status="published")
+    queryset = Car.objects.all()
     serializer_class = CarSerializer
-    # filterset_class = CarFilter
+
+    def list(self, request: Request, *args, **kwargs):
+        # queryset = self.filter_queryset(self.get_queryset())
+        check = check_user(request)
+        queryset = (Car.objects.filter(status='published'))
+        if check:
+            queryset = Car.objects.all()
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            self.permission_classes = [AllowAny]
+        elif self.request.method == "POST":
+            self.permission_classes = [IsAuthenticated]
+        else:
+            self.permission_classes = [IsOwner, IsAdminUser, IsModerator]
+        return super().get_permissions()
+    
 
 
-# class CarFilter(filters.FilterSet):
-#     min_price = filters.NumberFilter(field_name="status", lookup_expr='exact')
-
-#     class Meta:
-#         model = Car
-#         fields = ['status']
-
-# class CarViewSet(generics.ListAPIView):
-#     queryset = Car.objects.all()
-#     serializer_class = CarSerializer
-#     filter_backends = (filters.DjangoFilterBackend,)
-#     filterset_class = CarFilter
