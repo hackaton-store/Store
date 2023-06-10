e372c0ca3c5285a888f4b3c53eb547dafecb25b1
xDD <baisal2407@gmail.com>
2023-06-10 14:34:32 +0600

    updated permissions and added recommendations

--- car_reviews/urls.py
+++ car_reviews/urls.py
@@ -4,6 +4,7 @@ from car_reviews.views import CommentViewSet, RatingViewSet, SavedViewSet
 
 router = routers.DefaultRouter()
 
+
 router.register('comment', CommentViewSet, basename='comment')
 router.register('rating', RatingViewSet, basename='rating')
 router.register('saved', SavedViewSet, basename='saved')
